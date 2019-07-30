7bb2cce477e33057d36953b8bc26ef80cd4d5047
Brandon Koepke <bdkoepke@gmail.com>
2019-07-30 18:12:23 -0400

    Adding CredentialOData support.

---
+++ Client/CredentialOData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerQueryNet.Client
+{
+    /// <summary>
+    /// Credential to access an OData resource from the Power Query (M) formulas.
+    /// </summary>
+    public class CredentialOData : Credential
+    {
+        /// <summary>
+        /// URL address of the resource
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
